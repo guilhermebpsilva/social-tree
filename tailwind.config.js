5394753d09c07e5b4503587d1409a4ac79cf9dfa
guilhermebpsilva <gbpsilva0@gmail.com>
2022-11-19 07:41:22 -0300

    style refinements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*"],
   theme: {
     extend: {
       colors: {
-        bgPink: "#BB99DD"
+        mainPink: "#BB99DD",
+        bgWhite: "#E5E5E5",
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      backgroundImage: {
+        profilePic: "url('/assets/foto3x4.PNG')",
+      },
+      borderRadius: {
+        full: '50%'
+      },
+      minWidth: {
+        link: '300px'
+      },
+      maxWidth: {
+        icons: '300px'
       }
     },
   },
